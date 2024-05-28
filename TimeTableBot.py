00c2f21c9568063eed2c1f4276d5d4ce2b597a28
Infinity <155571616+IInfinityyyy@users.noreply.github.com>
2024-05-28 12:05:50 +0500

    The Best Update
    Bot can send homeworks to the users which they choose

--- TimeTableBot.py
+++ TimeTableBot.py
@@ -1,116 +1,107 @@
-import telebot
-from telebot import types
-import sqlite3
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-bot = telebot.TeleBot('tag')
-    
-@bot.message_handler(commands=['start'])
-def start(message):
-    conn = sqlite3.connect('ttbase.db')
-    cur = conn.cursor()
+DATABASE_URL = 'sqlite:///homework.db'
 
-    cur.execute('''
-CREATE TABLE IF NOT EXISTS ttbase (
-    f1 TEXT,
-    f2 TEXT,
-    f3 TEXT,
-    f4 TEXT,
-    f5 TEXT,
-    f6 TEXT,
-    f7 TEXT,
-    f8 TEXT,
-    f9 TEXT,
-    f10 TEXT,
-    f11 TEXT,
-    f12 TEXT,
-    f13 TEXT,
-    f14 TEXT,
-    f15 TEXT,
-    f16 TEXT,
-    f17 TEXT,
-    f18 TEXT,
-    f19 TEXT,
-    f20 TEXT
-)
-''')
-    conn.commit()
-    cur.close()
-    conn.close()
-    bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}, я TimeTableBot сокращено TTBot, выберите команду /TimeTable что бы увидеть расписание или выберите команду /HomeWork что бы посмотреть домашнее задание')
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
 
-@bot.message_handler(commands=['TimeTable'])
-def timetable(message):
-    markup = types.ReplyKeyboardMarkup()
-    monday = types.KeyboardButton('Понедельник')
-    thuesday = types.KeyboardButton('Четверг')
-    markup.row(monday, thuesday)
-    tuesday = types.KeyboardButton('Вторник')
-    friday = types.KeyboardButton('Пятница')
-    markup.row(tuesday, friday)
-    wedday = types.KeyboardButton('Среда')
-    satday = types.KeyboardButton('Суббота')
-    back = types.KeyboardButton('Назад')
-    markup.row(wedday, satday)
-    markup.row(back)
-    bot.send_message(message.chat.id, f'Выберите день недели', reply_markup=markup)
-    bot.register_next_step_handler(message, sendtt)
+class Homework(Base):
+    __tablename__ = 'homework'
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, unique=True, nullable=False)
+    homework = Column(Text, nullable=False)
 
-def sendtt(message):
-    if message.text == 'Понедельник':
-        bot.send_message(message.chat.id, f'\nРасписание на понедельник: \n1) <b>Информатика</b> | <b>Узбекский язык</b> 8:30 - 9:10 \n2) <b>Узбекский язык</b> | <b>Информатика</b> 9:15 - 9:55 \n3) <b>Алгебра</b> 10:05 - 10:45 \n4) <b>Химия</b> 11:00 - 11:40 \n5) <b>Русский язык</b> 11:45 - 12:25', parse_mode='html')
-    elif message.text == 'Вторник':
-        bot.send_message(message.chat.id, f'\nРасписание на вторник: \n1) <b>Геометрия</b> 8:30 - 9:10 \n2) <b>География</b> 9:15 - 9:55 \n3) <b>Литература</b> 10:05 - 10:45 \n4) <b>Физика</b> 11:00 - 11:40 \n5) <b>Физ-ра</b> 11:45 - 12:25 \n6) <b>Английский язык</b> 12:30 - 13:10', parse_mode='html')
-    elif message.text == 'Среда':
-        bot.send_message(message.chat.id, f'\nРасписание на cреду: \n1) <b>Русский язык</b> 8:30 - 9:10 \n2) <b>Алгебра</b> 9:15 - 9:55 \n3) <b>Биология</b> 10:05 - 10:45 \n4) <b>Экономика</b> 11:00 - 11:40 \n5) <b>Узбекский язык</b> 11:45 - 12:25 \n6) <b>Технология</b> 12:30 - 13:10', parse_mode='html')
-    elif message.text == 'Четверг':
-        bot.send_message(message.chat.id, f'\nРасписание на четверг: \n1) <b>Английский язык</b> 8:30 - 9:10 \n2) <b>Литература</b> 9:15 - 9:55 \n3) <b>Алгебра</b> 10:05 - 10:45 \n4) <b>История Узб.</b> 11:00 - 11:40 \n5) <b>Химия</b> 11:45 - 12:25 \n6) <b>Физ-ра</b> 12:30 - 13:10', parse_mode='html')
-    elif message.text == 'Пятница':
-        bot.send_message(message.chat.id, f'\nРасписание на пятницу: \n1) <b>Восп. час</b> 8:30 - 9:10 \n2) <b>Узбекский язык</b> 9:15 - 9:55 \n3) <b>Геометрия</b> 10:05 - 10:45 \n4) <b>Всемирная история</b> 11:00 - 11:40 \n5) <b>Биология</b> 11:45 - 12:25 \n6) <b>Воспитание</b> 12:30 - 13:10', parse_mode='html')
-    elif message.text == 'Суббота':
-        bot.send_message(message.chat.id, f'\nРасписание на субботу: \n1) <b>Черчение</b> 8:30 - 9:10 \n2) <b>Физика</b> 9:15 - 9:55 \n3) <b>История Узб.</b> 10:05 - 10:45 \n4) <b>Английский язык</b> 11:00 - 11:40 \n5) <b>Право</b> 11:45 - 12:25', parse_mode='html')
-    elif message.text == 'Воскресенье':
-        bot.send_message(message.chat.id, 'Хихи, а посхалки уже нет :^)')
-    elif message.text == 'Назад':
-        bot.register_next_step_handler(message, start)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#! Бот может отвечать в группе без доступа с сообщениям
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils import executor
+import logging
+
+API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
+# *Стартовая клавиатура
+main_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+main_keyboard.add(KeyboardButton('Увидеть школьное расписание'))
+main_keyboard.add(KeyboardButton('Увидеть домашнии задания'))
+
+# *Клавиатура с предметами
+subjects_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+subjects = ['Алгебра', 'Английский_язык', 'Биология', 'Воспит_час', 'Воспитание',
+            'Всемирная_история', 'География', 'Геометрия', 'Информатика',
+            'История_Узбекистана', 'Литература', 'Право', 'Русский_язык',
+            'Технология', 'Узбекский_язык', 'Физика', 'Физкультура', 'Химия',
+            'Черчение', 'Экономика']
+for i in range(0, len(subjects), 1):  # *Разбиваем на строки по 3 кнопки
+    subjects_keyboard.row(*[types.KeyboardButton(subject) for subject in subjects[i:i+1]])
+
+# *Обработчик команду start
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет я твой помощник по школе <b>TimeTableBot</b>, и помогу тебе узнавать расписание уроков и домашнии задания. Выбери действие:", reply_markup=main_keyboard, parse_mode='html')
+
+# *Школьное расписание
+@dp.message_handler(lambda message: message.text == "Увидеть школьное расписание")
+async def show_schedule(message: types.Message):
+    await message.reply("""
+\nРасписание на <b>Понедельник</b>: \n1) <b>Информатика</b> | <b>Узбекский язык</b> 8:30 - 9:10 \n2) <b>Узбекский язык</b> | <b>Информатика</b> 9:15 - 9:55 \n3) <b>Алгебра</b> 10:05 - 10:45 \n4) <b>Химия</b> 11:00 - 11:40 \n5) <b>Русский язык</b> 11:45 - 12:25
+\nРасписание на <b>Вторник</b>: \n1) <b>Геометрия</b> 8:30 - 9:10 \n2) <b>География</b> 9:15 - 9:55 \n3) <b>Литература</b> 10:05 - 10:45 \n4) <b>Физика</b> 11:00 - 11:40 \n5) <b>Физ-ра</b> 11:45 - 12:25 \n6) <b>Английский язык</b> 12:30 - 13:10
+\nРасписание на <b>Среду</b>: \n1) <b>Русский язык</b> 8:30 - 9:10 \n2) <b>Алгебра</b> 9:15 - 9:55 \n3) <b>Биология</b> 10:05 - 10:45 \n4) <b>Экономика</b> 11:00 - 11:40 \n5) <b>Узбекский язык</b> 11:45 - 12:25 \n6) <b>Технология</b> 12:30 - 13:10
+\nРасписание на <b>Четверг</b>: \n1) <b>Английский язык</b> 8:30 - 9:10 \n2) <b>Литература</b> 9:15 - 9:55 \n3) <b>Алгебра</b> 10:05 - 10:45 \n4) <b>История Узб.</b> 11:00 - 11:40 \n5) <b>Химия</b> 11:45 - 12:25 \n6) <b>Физ-ра</b> 12:30 - 13:10
+\nРасписание на <b>Пятницу</b>: \n1) <b>Восп. час</b> 8:30 - 9:10 \n2) <b>Узбекский язык</b> 9:15 - 9:55 \n3) <b>Геометрия</b> 10:05 - 10:45 \n4) <b>Всемирная история</b> 11:00 - 11:40 \n5) <b>Биология</b> 11:45 - 12:25 \n6) <b>Право</b> 12:30 - 13:10
+\nРасписание на <b>Субботу</b>: \n1) <b>Черчение</b> 8:30 - 9:10 \n2) <b>Физика</b> 9:15 - 9:55 \n3) <b>История Узб.</b> 10:05 - 10:45 \n4) <b>Английский язык</b> 11:00 - 11:40 \n5) <b>Воспитание</b> 11:45 - 12:25
+
+\np.s расписание можно закрепить что бы мне лишний раз его не отправлять 
+""", parse_mode='html')
+
+# *Выбор предмета
+@dp.message_handler(lambda message: message.text == "Увидеть домашнии задания")
+async def show_homework_options(message: types.Message):
+    await message.reply("Выбери предмет по которому хочешь увидеть домашнее задание.\nЧто бы изменить домашнее задание отправь сообщение следующим образом: <b>/upd Предмет Новое задание</b>\nЕсли предмет состоит из двух слов то между ними должен стоять знак \"_\"", reply_markup=subjects_keyboard, parse_mode='html')
+
+# *Отправка домашнего задания
+@dp.message_handler(lambda message: message.text in ['Алгебра', 'Английский_язык', 'Биология', 'Воспит_час', 'Воспитание',
+            'Всемирная_история', 'География', 'Геометрия', 'Информатика',
+            'История_Узбекистана', 'Литература', 'Право', 'Русский_язык',
+            'Технология', 'Узбекский_язык', 'Физика', 'Физкультура', 'Химия',
+            'Черчение', 'Экономика'])
+async def show_homework(message: types.Message):
+    subject = message.text
+    homework = session.query(Homework).filter_by(subject=subject).first()
+    if homework:
+        await message.reply(f"Домашнее задание по предмету {subject}: {homework.homework}")
     else:
-        bot.send_message(message.chat.id, 'Я вас не понимаю, пожалуйста выберите день недели')
-    bot.register_next_step_handler(message, sendtt)
+        await message.reply(f"Домашнее задание по предмету {subject} отсутствует")
 
-@bot.message_handler(commands=['HomeWork'])
-def homework(message):
-    conn = sqlite3.connect('ttbase.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM ttbase')
-    rows = cursor.fetchall()
-    conn.close()
+# *Функция изменения задания по предмету
+@dp.message_handler(commands=['upd'])
+async def update_homework(message: types.Message):
+    try:
+        _, subject, new_homework = message.text.split(' ', 2)
+        homework = session.query(Homework).filter_by(subject=subject).first()
+        if homework:
+            homework.homework = new_homework
+        else:
+            homework = Homework(subject=subject, homework=new_homework)
+            session.add(homework)
+        session.commit()
+        await message.reply(f"Домашнее задание по предмету {subject} обновлено")
+    except ValueError:
+        await message.reply("Напоминаю, для изменение домашнего задания отправь сообщение таким образом: <b>/upd Предмет Новое задание</b>")
 
-    markup = types.ReplyKeyboardMarkup()
-
-    algebra = types.KeyboardButton('Алгебра')
-    english = types.KeyboardButton('Английский язык')
-    biology = types.KeyboardButton('Биология')
-    edu_h = types.KeyboardButton('Восп. час')
-    edu = types.KeyboardButton('Воспитание')
-    wwhistory = types.KeyboardButton('Всемирная история')
-    geography = types.KeyboardButton('География')
-    geometry = types.KeyboardButton('Геометрия')
-    informatics = types.KeyboardButton('Информатика')
-    uzhistory = types.KeyboardButton('История Узбекистана')
-    liter = types.KeyboardButton('Литература')
-    pravo = types.KeyboardButton('Право')
-    russ = types.KeyboardButton('Русский язык')
-    tech = types.KeyboardButton('Технология')
-    uzb = types.KeyboardButton('Узбекский язык')
-    physic = types.KeyboardButton('Физика')
-    fizra = types.KeyboardButton('Физкультура')
-    himiya = types.KeyboardButton('Химия')
-    cher = types.KeyboardButton('Черчение')
-    economy = types.KeyboardButton('Экономика')
-    bot.send_message(message.chat.id, f'Выберите предмет', reply_markup=markup)
-
-    for row in rows:
-        if message.text == 'Алгебра':
-            print(row[0])
-
-    
-bot.infinity_polling()
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+# !Расширение Better Commands для пророботанных комментариев
